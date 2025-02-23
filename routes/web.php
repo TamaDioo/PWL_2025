7178921d0131014ef53df1dffffd128b40df2180
Dio Andika Pradana Mulia Tama <tamadio91@gmail.com>
2025-02-23 20:56:52 +0700

    Belajar Routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,52 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/about', function () {
+    return 'NIM: 2341720098 <br> 
+            Nama: Dio Andika Pradana Mulia Tama';
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '. $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '. $id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+
+// Contoh Route Name:
+Route::get('/user/profile', function () {
+    //
+})->name('profile');
+
+// Route::get(
+//     '/user/profile',
+//     [UserProfileController::class, 'show']
+// )->name('profile');
+    
+// Generating URLs...
+$url = route('profile');
+
+// Generating Redirects...
+return redirect()->route('profile');
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +59,53 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route di bawah ini harus di non aktifkan agar tidak konflik
+// dengan route '/' yang baru.
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Selamat Datang';
 });
+
+/* 
+// Contoh Route Group:
+Route::middleware(['first', 'second'])->group(function () {
+    Route::get('/', function () {
+        // Uses first & second middleware...
+    });
+
+Route::get('/user/profile', function () {
+        // Uses first & second middleware...
+    });
+});
+
+Route::domain('{account}.example.com')->group(function () {
+    Route::get('user/{id}', function ($account, $id) {
+        //
+    });
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
+*/
+
+/*
+// Contoh Route Prefixes
+Route::prefix('admin')->group(function () {
+    Route::get('/user', [UserController::class, 'index']);
+    Route::get('/post', [PostController::class, 'index']);
+    Route::get('/event', [EventController::class, 'index']);
+});
+*/
+
+//Contoh Redirect Routes
+Route::redirect('/here', '/there');
+
+// Contoh View Routes
+Route::view('/welcome', 'welcome');
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
