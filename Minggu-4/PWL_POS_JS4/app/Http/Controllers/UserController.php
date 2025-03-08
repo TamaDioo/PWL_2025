f194692c917ec2dccee93feaf30723567533e56f
Dio Andika Pradana Mulia Tama <tamadio91@gmail.com>
2025-03-08 21:53:54 +0700

    add: Praktikum 1 -  Jobsheet 4

--- Minggu-4/PWL_POS_JS4/app/Http/Controllers/UserController.php
+++ Minggu-4/PWL_POS_JS4/app/Http/Controllers/UserController.php
@@ -10,6 +10,18 @@ class UserController extends Controller
 {
     public function index()
     {
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_3',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
+
+        // coba akses model UserModel
+        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+
         // tambah data user dengan Eloquent Model
         // $data = [
         //     'username' => 'customer-1',
@@ -19,14 +31,10 @@ class UserController extends Controller
         // ];
         // UserModel::insert($data); // tambahkan data ke tabel m_user
 
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); // update data user
-
-        // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data); // update data user
     }
 
     // Method ini akan menampilkan profil pengguna 
