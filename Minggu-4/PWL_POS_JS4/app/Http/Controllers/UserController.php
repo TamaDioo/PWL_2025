b3c8e7d37e0785e1c7467b26d72bd26b85ca1743
Dio Andika Pradana Mulia Tama <tamadio91@gmail.com>
2025-03-09 10:00:53 +0700

    add: Praktikum 2.1 – Retrieving Single Models

--- Minggu-4/PWL_POS_JS4/app/Http/Controllers/UserController.php
+++ Minggu-4/PWL_POS_JS4/app/Http/Controllers/UserController.php
@@ -10,17 +10,28 @@ class UserController extends Controller
 {
     public function index()
     {
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_3',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $user = UserModel::find(1); // Hanya mengambil 1 data user dengan PK = 1
+        // $user = UserModel::firstWhere('level_id', 1);
+        // $user = UserModel::where('level_id', 1)->first();
+        // $user = UserModel::findOr(1, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
+        return view('user', ['data' => $user]);
+
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_3',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
 
         // coba akses model UserModel
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+        // $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
 
         // tambah data user dengan Eloquent Model
         // $data = [
