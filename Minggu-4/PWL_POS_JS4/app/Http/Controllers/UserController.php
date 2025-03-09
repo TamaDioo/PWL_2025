4e5e45b729670187d0e37add42cd230f7d730272
Dio Andika Pradana Mulia Tama <tamadio91@gmail.com>
2025-03-09 10:20:27 +0700

    add: Praktikum 2.2 – Not Found Exceptions

--- Minggu-4/PWL_POS_JS4/app/Http/Controllers/UserController.php
+++ Minggu-4/PWL_POS_JS4/app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -10,16 +11,18 @@ class UserController extends Controller
 {
     public function index()
     {
+        // $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        return view('user', ['data' => $user]);
         // $user = UserModel::find(1); // Hanya mengambil 1 data user dengan PK = 1
         // $user = UserModel::firstWhere('level_id', 1);
         // $user = UserModel::where('level_id', 1)->first();
         // $user = UserModel::findOr(1, ['username', 'nama'], function () {
         //     abort(404);
         // });
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
-        return view('user', ['data' => $user]);
+        // $user = UserModel::findOr(20, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
 
         // $data = [
         //     'level_id' => 2,
