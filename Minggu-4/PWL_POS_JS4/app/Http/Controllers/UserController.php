962394a7597c98dd9b935e06d60e6337a456fddf
Dio Andika Pradana Mulia Tama <tamadio91@gmail.com>
2025-03-09 11:12:37 +0700

    add: Praktikum 2.3 – Retreiving Aggregrates

--- Minggu-4/PWL_POS_JS4/app/Http/Controllers/UserController.php
+++ Minggu-4/PWL_POS_JS4/app/Http/Controllers/UserController.php
@@ -11,8 +11,9 @@ class UserController extends Controller
 {
     public function index()
     {
-        // $user = UserModel::findOrFail(1);
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
+        $user = UserModel::where('level_id', 2)->count();
         return view('user', ['data' => $user]);
         // $user = UserModel::find(1); // Hanya mengambil 1 data user dengan PK = 1
         // $user = UserModel::firstWhere('level_id', 1);
@@ -23,6 +24,8 @@ class UserController extends Controller
         // $user = UserModel::findOr(20, ['username', 'nama'], function () {
         //     abort(404);
         // });
+        // $user = UserModel::findOrFail(1);
+        // $user = UserModel::where('username', 'manager9')->firstOrFail();
 
         // $data = [
         //     'level_id' => 2,
