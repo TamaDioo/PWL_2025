da63618edd17abaf4ae9559c1bc7ba7bd932674d
Dio Andika Pradana Mulia Tama <tamadio91@gmail.com>
2025-03-09 18:57:32 +0700

    add: Praktikum 2.6 – Create, Read, Update, Delete (CRUD)

--- Minggu-4/PWL_POS_JS4/app/Http/Controllers/UserController.php
+++ Minggu-4/PWL_POS_JS4/app/Http/Controllers/UserController.php
@@ -9,46 +9,51 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+
     public function index()
     {
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+
+        /*Praktikum 2.5
         $user = UserModel::create([
             'username' => 'manager11',
             'nama' => 'Manager11',
             'password' => Hash::make('12345'),
             'level_id' => 2,
         ]);
-
+    
         $user->username = 'manager12';
-
+    
         $user->save();
-
+    
         $user->wasChanged(); // true
         $user->wasChanged('username'); // true
         $user->wasChanged(['username', 'level_id']); // true
         $user->wasChanged(['nama']); // false
         dd($user->wasChanged(['nama', 'username'])); // true
-
-        /*$user = UserModel::create([
+    
+        $user = UserModel::create([
             'username' => 'manager55',
             'nama' => 'Manager55',
             'password' => Hash::make('12345'),
             'level_id' => 2,
         ]);
-
+    
         $user->username = 'manager56';
-
+    
         $user->isDirty(); //true
         $user->isDirty('username'); //true
         $user->isDirty('nama'); //false
         $user->isDirty(['nama', 'username']); //true
-
+    
         $user->isClean(); //false
         $user->isClean('username'); //false
         $user->isClean('nama'); //true
         $user->isClean(['nama', 'username']); //false
-
+    
         $user->save();
-
+    
         $user->isDirty(); //false
         $user->isClean(); //true
         dd($user->isDirty()); */
@@ -65,7 +70,7 @@ class UserController extends Controller
             ],
         );
         $user->save();
-
+    
         return view('user', ['data' => $user]); */
 
         // $user = UserModel::firstOrCreate(
@@ -127,6 +132,51 @@ class UserController extends Controller
         //     'nama' => 'Pelanggan Pertama',
         // ];
         // UserModel::where('username', 'customer-1')->update($data); // update data user
+    }
+
+    public function tambah()
+    {
+        return view('user_tambah');
+    }
+
+    public function tambah_simpan(Request $request)
+    {
+        UserModel::create([
+            'username' => $request->username,
+            'nama' => $request->nama,
+            'password' => Hash::make('$request->password'),
+            'level_id' => $request->level_id,
+        ]);
+
+        return redirect('/user');
+    }
+
+    public function ubah($id)
+    {
+        $user = UserModel::find($id);
+        return view('user_ubah', ['data' => $user]);
+    }
+
+    public function ubah_simpan($id, Request $request)
+    {
+        $user = UserModel::find($id);
+
+        $user->username = $request->username;
+        $user->nama = $request->nama;
+        $user->password = Hash::make('$request->password');
+        $user->level_id = $request->level_id;
+
+        $user->save();
+
+        return redirect('/user');
+    }
+
+    public function hapus($id)
+    {
+        $user = UserModel::find($id);
+        $user->delete();
+
+        return redirect('/user');
     }
 
     // Method ini akan menampilkan profil pengguna 
