a539a4c7f6933d277700403c998f88f58d815bfb
Dio Andika Pradana Mulia Tama <tamadio91@gmail.com>
2025-03-10 18:52:30 +0700

    Menambahkan kembali baris kode praktikum sebelumnya yang telah dihapus/diganti

--- Minggu-4/PWL_POS_JS4/app/Models/UserModel.php
+++ Minggu-4/PWL_POS_JS4/app/Models/UserModel.php
@@ -18,6 +18,7 @@ class UserModel extends Model
      * @var array
      */
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    // protected $fillable = ['level_id', 'username', 'nama']; // Praktikum 1 Langkah 4
 
     public function level(): BelongsTo
     {
