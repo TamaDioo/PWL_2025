5c5c30e3846d0a71c0c31199176616464cd45f1c
Dio Andika Pradana Mulia Tama <tamadio91@gmail.com>
2025-03-16 16:40:06 +0700

    add: Praktikum 2 – Integrasi dengan DataTables

--- Minggu-5/PWL_POS_JS5/database/seeders/KategoriSeeder.php
+++ Minggu-5/PWL_POS_JS5/database/seeders/KategoriSeeder.php
@@ -11,11 +11,13 @@ class KategoriSeeder extends Seeder
     public function run(): void
     {
         $data = [
-            ['kategori_id' => 1, 'kategori_kode' => 'ELCT', 'kategori_nama' => 'Elektronik'],
-            ['kategori_id' => 2, 'kategori_kode' => 'FSHN', 'kategori_nama' => 'Pakaian'],
-            ['kategori_id' => 3, 'kategori_kode' => 'FOOD', 'kategori_nama' => 'Makanan'],
-            ['kategori_id' => 4, 'kategori_kode' => 'BVRG', 'kategori_nama' => 'Minuman'],
-            ['kategori_id' => 5, 'kategori_kode' => 'BOOK', 'kategori_nama' => 'Buku'],
+            // ['kategori_id' => 1, 'kategori_kode' => 'ELCT', 'kategori_nama' => 'Elektronik'],
+            // ['kategori_id' => 2, 'kategori_kode' => 'FSHN', 'kategori_nama' => 'Pakaian'],
+            // ['kategori_id' => 3, 'kategori_kode' => 'FOOD', 'kategori_nama' => 'Makanan'],
+            // ['kategori_id' => 4, 'kategori_kode' => 'BVRG', 'kategori_nama' => 'Minuman'],
+            // ['kategori_id' => 5, 'kategori_kode' => 'BOOK', 'kategori_nama' => 'Buku'],
+            ['kategori_id' => 6, 'kategori_kode' => 'CML', 'kategori_nama' => 'Cemilan', 'created_at' => now()],
+            ['kategori_id' => 7, 'kategori_kode' => 'MNR', 'kategori_nama' => 'Minuman Ringan', 'created_at' => now()],
         ];
 
         DB::table('m_kategori')->insert($data);
