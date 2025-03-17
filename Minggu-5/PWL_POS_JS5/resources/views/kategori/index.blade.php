51df1fe81de5962847ed459eb59f0384a033f29b
Dio Andika Pradana Mulia Tama <tamadio91@gmail.com>
2025-03-17 16:08:24 +0700

    add: Tombol Tambah Kategori (Tugas Praktikum No 1)

--- Minggu-5/PWL_POS_JS5/resources/views/kategori/index.blade.php
+++ Minggu-5/PWL_POS_JS5/resources/views/kategori/index.blade.php
@@ -9,7 +9,10 @@
 @section('content')
     <div class="container">
         <div class="card">
-            <div class="card-header">Manage Kategori</div>
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <h5 class="mb-0">Manage Kategori</h5>
+                <a href="{{ url('/kategori/create') }}" class="btn btn-primary btn-md ms-auto">Tambah Kategori</a>
+            </div>
             <div class="card-body">
                 {{ $dataTable->table() }}
             </div>
