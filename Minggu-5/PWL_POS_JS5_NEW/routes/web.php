3539c6ee9d69149452bdd85e5750fbe0cea41e22
Dio Andika Pradana Mulia Tama <tamadio91@gmail.com>
2025-03-24 18:53:20 +0700

    add: Praktikum 2 Jobsheet 5 (Baru)

--- Minggu-5/PWL_POS_JS5_NEW/routes/web.php
+++ Minggu-5/PWL_POS_JS5_NEW/routes/web.php
@@ -7,9 +7,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SalesController;
+use App\Http\Controllers\WelcomeController;
 
 // Route untuk Halaman Home
-Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/', [WelcomeController::class, 'index']);
+// Route::get('/', [HomeController::class, 'index'])->name('home');
 
 // Route untuk Halaman Products menggunakan Route Prefix
 Route::prefix('category')->group(function () {
