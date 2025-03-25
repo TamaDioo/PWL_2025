2953959f7c4a72a084d0814cd7f83f1e29374750
Dio Andika Pradana Mulia Tama <tamadio91@gmail.com>
2025-03-25 20:37:08 +0700

    add: Praktikum 3 Jobsheet 5 (Baru)

--- Minggu-5/PWL_POS_JS5_NEW/routes/web.php
+++ Minggu-5/PWL_POS_JS5_NEW/routes/web.php
@@ -2,15 +2,26 @@
 
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
-use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SalesController;
 use App\Http\Controllers\WelcomeController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-// Route untuk Halaman Home
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']);          // menampilkan halaman awal user
+    Route::post('/list', [UserController::class, 'list']);      // menampilkan data user untuk datatables (JSON)
+    Route::get('/create', [UserController::class, 'create']);   // menampilkan halaman form tambah user
+    Route::post('/', [UserController::class, 'store']);         // menyimpan data user baru
+    Route::get('/{id}', [UserController::class, 'show']);       // manampilkan detail user
+    Route::get('/{id}/edit', [UserController::class, 'edit']);  // menampilkan halaman form edit user
+    Route::put('/{id}', [UserController::class, 'update']);     // menyimpan perubahan data user
+    Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+});
+
 // Route::get('/', [HomeController::class, 'index'])->name('home');
 
 // Route untuk Halaman Products menggunakan Route Prefix
