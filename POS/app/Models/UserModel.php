c9d83689d3eedc1757915e98bb8f7046a89bed5c
Dio Andika Pradana Mulia Tama <tamadio91@gmail.com>
2025-04-05 15:09:39 +0700

    add: Praktikum 1 Jobsheet 7

--- POS/app/Models/UserModel.php
+++ POS/app/Models/UserModel.php
@@ -5,21 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User as Authenticatable; // implementasi class Authenticatable
 
-class UserModel extends Model
+class UserModel extends Authenticatable
 {
     use HasFactory;
 
     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
-    /**
-     * The attributes that are mass assignable.
-     * 
-     * @var array
-     */
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $fillable = ['username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
+
+    protected $hidden = ['password']; // jangan ditampilkan saat select
+
+    protected $casts = ['password' => 'hashed']; // casting password agar otomatis di hash
     // protected $fillable = ['level_id', 'username', 'nama']; // Praktikum 1 Langkah 4
 
+    /**
+     * Relasi ke tabel level
+     */
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
