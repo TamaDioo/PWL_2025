0fff5cb2f3f129bde8c863aa443e453d803e1892
Dio Andika Pradana Mulia Tama <tamadio91@gmail.com>
2025-04-27 20:56:33 +0700

    add: Praktikum 2 – Membuat RESTful API Login

--- Minggu-10/PWL_POS_JS10/routes/api.php
+++ Minggu-10/PWL_POS_JS10/routes/api.php
@@ -13,8 +13,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+use App\Http\Controllers\Api\LoginController;
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
